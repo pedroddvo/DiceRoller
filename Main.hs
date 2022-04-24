809256ec5de9a2be5e5f4d2af8359d3e6649f75a
pedroddvo <pedroddvo@gmail.com>
2022-04-24 01:50:03 +0100

    Game

---
+++ Main.hs
@@ -0,0 +1,57 @@
+{-# LANGUAGE LambdaCase #-}
+module Main where
+
+import System.Random
+import Control.Monad.State
+import Data.Time.Clock
+import Data.Maybe
+
+pureGen :: IO StdGen
+pureGen =  mkStdGen
+        .  floor
+        .  toRational
+        .  utctDayTime
+       <$> getCurrentTime
+
+roll :: IO Int
+roll = getStdRandom $ randomR (0, 100)
+
+data Guess = Start
+           | Guessed Int
+
+guess :: Int -> Guess -> IO ()
+guess r g = do
+    case g of
+        Start -> do
+            mnum <- fmap fst . listToMaybe . reads <$> getLine
+            case mnum of
+                Nothing -> do
+                    putStrLn "Bad input! Enter a number 0-100"
+                    guess r Start
+                Just num -> guess r (Guessed num)
+        Guessed n -> do
+            res <- guessIt n r
+            if res
+                then return ()
+                else guess r Start
+
+    where
+        guessIt n rng
+            | n == rng = do
+                putStrLn "You guessed right!"
+                return True
+            | n > rng  = do
+                putStrLn "You're over!"
+                return False
+            | n < rng  = do
+                putStrLn "You're under!"
+                return False
+
+game :: IO ()
+game = do
+    putStrLn "Enter a valid number"
+    r <- roll
+    guess r Start
+
+main :: IO ()
+main = undefined
---
+++ WithState.hs
@@ -0,0 +1,65 @@
+module WithState where
+
+import System.Random ( randomRIO )
+import Control.Monad.State.Lazy
+import Data.Maybe
+
+data GameRecord =
+     GameRecord { rng            :: Int
+                , guesses        :: Int
+                , correctGuesses :: Int }
+
+type GameState = StateT GameRecord IO
+
+roll :: IO Int
+roll = randomRIO (0, 100)
+
+input :: GameState Int
+input = do
+    liftIO $ putStrLn "Enter a number from 0 - 100"
+    num <- liftIO (fmap fst . listToMaybe . reads <$> getLine)
+    maybe input return num
+
+guess :: Bool -> GameState ()
+guess True  = return ()
+guess False = do
+    n <- input
+    s <- get
+    compareNums (rng s) n s
+    s' <- get
+    if correctGuesses s' == 1
+        then guess True
+        else guess False
+
+    where
+        compareNums r n s
+            | n == r = do
+                liftIO $ putStrLn "Correct!"
+                liftIO $ putStrLn $ "That took you this many tries: " ++ show (guesses s)
+                r' <- liftIO roll
+                put GameRecord { rng            = r'
+                               , correctGuesses = correctGuesses s + 1
+                               , guesses        = 0 }
+            | n > r  = do
+                liftIO $ putStrLn "You're over!"
+                incGuess
+            | n < r  = do
+                liftIO $ putStrLn "You're under!"
+                incGuess
+
+        incGuess =
+            modify (\s -> GameRecord { rng            = rng s
+                                     , correctGuesses = correctGuesses s
+                                     , guesses        = guesses s + 1 })
+
+    -- if n ==
+    --
+emptyGame :: GameRecord
+emptyGame = GameRecord { rng            = 0
+                       , correctGuesses = 0
+                       , guesses        = 0 }
+
+game :: IO GameRecord
+game = do
+    r <- roll
+    snd <$> runStateT (guess False) emptyGame
